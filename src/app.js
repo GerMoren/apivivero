7428e69b4ba14ee44450db5dbdcf2126a3b4e98a
GerMoren <ingenieriamg91@gmail.com>
2020-10-07 10:48:25 -0300

    instal sesssion and cookie

--- src/app.js
+++ src/app.js
@@ -89,9 +89,11 @@ server.use(morgan("dev"));
 
 server.use(
   session({
+    store: new (require("connect-pg-simple")(session))(),
     secret: "clasificado",
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
   })
 );
 
